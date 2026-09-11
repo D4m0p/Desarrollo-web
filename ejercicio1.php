d30b430533a403f0bd96d627bb2d06bf89d28dab
Damo <dmeli8930@gmail.com>
2026-09-11 08:21:00 -0500

    Corrige el saludo segun la hora y usa la zona horaria de Panama

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,5 +1,9 @@
 <?php
-    if (date ('a' == 'pm')) {
+    // Sin esta línea PHP usa la hora UTC, 5 horas por delante de Panamá
+    date_default_timezone_set('America/Panama');
+
+    // date('a') devuelve "am" o "pm"; la comparación va fuera de los paréntesis de date()
+    if (date('a') == 'pm') {
         $saludo = 'Buenas tardes/noches!';
     } else {
         $saludo = 'Buenos dias!';
